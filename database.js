92303a8fa27aba5581e05fe797903fb45b55ae9e
lock0 <xiaoxiao95888@163.com>
2015-11-30 17:47:33 +0800

    Initial commit

---
+++ database.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Ben on 2015/11/30.
+ */
+var sql = require("mssql");
+var dbconfig = {
+    server: "localhost",
+    database: "SMCSFE",
+    user: "sa",
+    password: "XXXboy123",
+    port: 1433
+};
+
+function getBrands(callback){
+    var conn = new sql.Connection(dbconfig);
+    var req = new sql.Request(conn);
+    conn.connect().then(function () {
+        req.query("select * from Brands").then(function (recode) {
+            callback(recode);
+            conn.close();
+        }).catch(function(err) {
+            callback(err);
+            conn.close();
+        });
+    }).catch(function (err) {
+       return err;
+    });
+}
+function getBrand(callback,id){
+    var conn = new sql.Connection(dbconfig);
+    var req = new sql.Request(conn);
+    req.input('id', sql.NVarChar(), id);
+    conn.connect().then(function () {
+        req.query('select * from Brands where Id =@id').then(function (recode) {
+            callback(recode);
+            conn.close();
+        }).catch(function(err) {
+            callback(err);
+            conn.close();
+        });
+    }).catch(function (err) {
+        return err;
+    });
+}
+exports.getBrands = getBrands;
+exports.getBrand = getBrand;
---
+++ routes/api/brand.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Ben on 2015/11/30.
+ */
+var express = require('express');
+var router = express.Router();
+var db = require("./../../database");
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    db.getBrands(function(data){
+        res.send(data);
+    });
+});
+router.get('/:id', function(req, res, next) {
+    var id = req.params.id;
+    db.getBrand(function(data){
+        res.send(data);
+    },id);
+});
+router.post('/', function(req, res, next) {
+    res.send(req.body);
+});
+module.exports = router;
